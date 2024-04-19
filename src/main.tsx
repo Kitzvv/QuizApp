0c5426d4691fcd871f3f8bfe7ec7f702122db546
Kitzvv <michalnowakowski02@gmail.com>
2024-04-19 20:42:41 +0200

    Fixed main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,9 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
-
-// ReactDOM.createRoot(document.getElementById("root")!).render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <App />
-//     </Provider>
-//   </React.StrictMode>
-// );
